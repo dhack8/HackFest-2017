45ada5a1d8985df891c63fc55b6a6d7bf9c1beee
David Hack <dhack8@gmail.com>
2017-08-12 13:35:18 +1200

    Edited html, Added ID and Secret

--- oauth.js
+++ oauth.js
@@ -1,8 +1,8 @@
 var ids = {
   instagram: {
-    clientID: 'get_your_own',
-    clientSecret: 'get_your_own',
-    callbackURL: 'http://127.0.0.1:1337/auth/instagram/callback'
+    clientID: '2baab622a9d44a9d962742f3ba2ae74d',
+    clientSecret: 'd5145e8c983745f0bef2263a20f9d9bc',
+    callbackURL: 'http://127.0.0.1:3000/auth/instagram/callback'
   }
 };
 
