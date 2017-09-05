5693879a13c65fb84df05e55b802a07f0c8e2bdf
Brady Hanna <hannabradyw@gmail.com>
2017-09-05 19:21:07 +1200

    Tidied up the nodejs backend, added comments

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+/*
+  This is the backend of our code that processes data from the
+  instagram API and serves endpoints to our frontend. 
+
+*/
+
 var http = require('http');
 var express = require('express');
 var api = require('instagram-node').instagram();
 var app = express();
-
 var latestpostlikes = require('./latestpostlikes.json');
-
 var port = process.env.PORT || 3000;
 
-
-console.log(latestpostlikes.posts[0].id);
+/*
+  Ideally each of our endpoints would be able to query the
+  Instagram API, process the JSON data retrieved from it
+  and then send the processed data to our frontend. However
+  due to issues getting OAuth working across queries our
+  endpoints have a default fallback hardcoded value. For
+  future improvements we would want to fix this OAuth
+  issue and tidy up the code surrounding our attempt to
+  get this working as currently, in the nature of this
+  code been written at a hackfest, it is very hacked 
+  together in an attempt to get it working. Currently we
+  can query and retrieve data from the Instagram API when
+  manually navigating to the authorizing, but when deployed
+  as an app the system doesnt function properly.
+*/
 
 app.get('/latestpostlikes', function(req, res) {
   res.send(latestpostlikes.posts[0]);
@@ -19,125 +36,42 @@ var mediaSaved;
 var followersSaved;
 var isLoggedIn;
 
-function populateData (req,res) {
-  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
-    if (err) {
-      //console.log(err.body);
-      res.send("Didn't work");
-    } else {
-      isLoggedIn = true;
-      console.log("Connection succeeded. Access token is"  + result.access_token);
-      api.use({
-        access_token: result.access_token
-      });
-      console.log(result.access_token);
-
-      api.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
-       mediaSaved = medias;
-       //console.log(medias);
-       //console.log(mediaSaved);
-
-        api.user_followers(medias[0].user.id, function(err, users, pagination, remaining, limit) {
-          followersSaved = users;
-          // console.log(err);
-          // console.log(users);
-
-          var followers = users;
-
-          var likes = medias[0].likes;
-          var comments = medias[0].comments;
-          var tags = medias[0].tags;
-
-
-          var top5 = JSON.parse(JSON.stringify(medias));
-          top5.sort(function(a, b) {
-            return b.likes.count - a.likes.count;
-          });
-          top5 = top5.slice(0, 5);
-
-          var totalLikes = medias.reduce(function(sum, value) {
-            return sum + value.likes.count;
-          }, 0);
-
-          var totalComments = medias.reduce(function(sum, value) {
-            return sum + value.comments.count;
-          }, 0);
-
-          var variables = {medias, followers, likes, comments, tags, top5, totalLikes, totalComments};
-
-          //res.render("index.html.ejs", variables);
-        });
-
-      });
-
-    }
-  });
-}
-
-// var calclikestoday = function(){
-
-//   api.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
-//     var totalLikes = medias.reduce(function(sum, value) {
-//             return sum + value.likes.count;
-//           }, 0);
-
-
-//     return totalLikes;
-//   });
-// }
-
 
 app.get('/timeofmostrecentpost', function(req,res){
-
-
   var time = 1502594035;
-
   res.send({time});
 });
 
 
 var prevLikes = 2;
-
 app.get('/likestoday', function(req,res){
     if(!mediaSaved){
       var likesTdy = 5;
       res.send({likesTdy});
     }
     else{
-
-
     var totalLikes = mediaSaved.reduce(function(sum, value) {
             return sum + value.likes.count;
           }, 0);
-
-
     var likesTdy = totalLikes - prevLikes;
-
     res.send({likesTdy});
   }
 });
 
 var prevComments = 3;
-
 app.get('/commentstoday', function(req,res){
     if(!mediaSaved){
       var commentsTdy= 6;
       res.send({commentsTdy});
     }
     else{
-
     var totalComments = mediaSaved.reduce(function(sum, value) {
             return sum + value.comments.count;
           }, 0);
-
-
     var commentsTdy = totalComments - prevComments;
-
     res.send({commentsTdy});
   }
 });
-
-
 
 app.get('/totallikes', function(req,res){
  if(!mediaSaved){
@@ -145,15 +79,11 @@ app.get('/totallikes', function(req,res){
       res.send({totalLikes});
     }
 else{
-
-
     var totalLikes = mediaSaved.reduce(function(sum, value) {
             return sum + value.likes.count;
           }, 0);
-
     res.send({totalLikes});
   }
-
 });
 
 app.get('/totalcomments', function(req,res){
@@ -165,8 +95,6 @@ app.get('/totalcomments', function(req,res){
     var totalComments = mediaSaved.reduce(function(sum, value) {
             return sum + value.comments.count;
           }, 0);
-
-
     res.send({totalComments});
   }
 });
@@ -175,19 +103,13 @@ app.get('/totalcomments', function(req,res){
 var prevFollowers = 1;
 app.get('/followerstoday', function(req,res){
 if(!followersSaved){
-
-
     var totfol = 2;
     res.send({totfol});
 }
 else{
-
   var totfol = followersSaved.length - prevFollowers;
-
   res.send({totfol});
 }
-
-
 });
 
 
@@ -202,22 +124,17 @@ app.get('/totalfollowers', function(req,res){
 
   res.send({totfol});
   }
-
-
 });
 
 
 app.get('/engagementtoday', function(req, res){
-
   var engtoday = 34;
   res.send({engtoday});
-
 });
 
 app.get('/totalengagement', function(req, res){
-   var toteng = 151;
+  var toteng = 151;
   res.send({toteng});
-
 });
 
 
@@ -268,45 +185,28 @@ var handleauth = function(req, res) {
         access_token: result.access_token
       });
       console.log(result.access_token);
-
       api.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
        mediaSaved = medias;
-       //console.log(medias);
-       //console.log(mediaSaved);
-
         api.user_followers(medias[0].user.id, function(err, users, pagination, remaining, limit) {
           followersSaved = users;
-          // console.log(err);
-          // console.log(users);
-
           var followers = users;
-
           var likes = medias[0].likes;
           var comments = medias[0].comments;
           var tags = medias[0].tags;
-
-
           var top5 = JSON.parse(JSON.stringify(medias));
           top5.sort(function(a, b) {
             return b.likes.count - a.likes.count;
           });
           top5 = top5.slice(0, 5);
-
           var totalLikes = medias.reduce(function(sum, value) {
             return sum + value.likes.count;
           }, 0);
-
           var totalComments = medias.reduce(function(sum, value) {
             return sum + value.comments.count;
           }, 0);
-
           var variables = { medias, followers, likes, comments, tags, top5, totalLikes, totalComments };
-
-          //res.render("index.html.ejs", variables);
         });
-
       });
-
     }
   });
 };
@@ -317,7 +217,6 @@ app.get('/authorize_user', authorize_user);
 app.get('/handleauth', handleauth);
 
 app.get('/', function(req, res) {
-  //res.sendFile('static/index.html');
   res.redirect('/authorize_user');
 });
 
@@ -326,6 +225,7 @@ app.listen(port, function () {
 });
 
 
+// used to convert from 24hour to am/pm
 const TIMES_MAP = {
         "00": "12 am",
         "01": "1 am",
